aa9e772cc2bc8d8f542413be08ed0e6b05e4a2b2
Indra Maulana Agung <73073098+indramaulanaagung@users.noreply.github.com>
2024-09-23 03:32:14 +0700

    Update Navbar.js

--- src/pages/commponent/Navbar.js
+++ src/pages/commponent/Navbar.js
@@ -1,24 +1,23 @@
 import React from 'react'
-import { useNavigate } from "react-router-dom";
-import { BrowserRouter as Router, Routes,  Route, Link } from "react-router-dom";
-import Login from '../Auth/Login';
+import { useNavigate, navigate } from "react-router-dom";
+const NavbarSc = () => {
 
-const Navbar = () => {
-
+  const navigate = useNavigate();
+  
   const goToLogin = () => {
-    navigate("/Login");
+    navigate("Login");
   };
 
   return (
-
-<nav class=" border-gray-200 absolute w-full z-10">
+   
+<nav class="bg-white border-gray-200 fixed w-full ">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <Link to='/Login' relative='path'/>
-      <span class="self-center text-2xl font-semibold whitespace-nowrap text-black">SlyBuild</span>
+
+      
+      <span class="self-center text-2xl font-semibold whitespace-nowrap">SlyBuild</span>
   
   <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-  
-  <button onClick={()=>goToLogin()}type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+      <button onClick={()=>goToLogin()}type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
       <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -27,26 +26,24 @@ const Navbar = () => {
     </button>
   </div>
   <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
+    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
       <li>
-        <a href="#" class="block py-2 px-3 md:p-0  bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">Home</a>
+        <a href="#" class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">Home</a>
       </li>
       <li>
-        <a href="#" class="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
+        <a href="#" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-whitedark:hover:text-white md:dark:hover:bg-transparent">About</a>
       </li>
       <li>
-        <a href="#" class="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
+        <a href="#" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-whitedark:hover:text-white md:dark:hover:bg-transparent">Services</a>
       </li>
       <li>
-        <a href="#" class="block py-2 px-3 md:p-0 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
+        <a href="#" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-whitedark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
       </li>
     </ul>
   </div>
   </div>
 </nav>
-
-
   )
 }
 
-export default Navbar
+export default NavbarSc
