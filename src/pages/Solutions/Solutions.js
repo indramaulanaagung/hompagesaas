5b174b88fc99a5923ac5b12e12ab303fd60f187c
Indra Maulana Agung <73073098+indramaulanaagung@users.noreply.github.com>
2024-09-23 03:39:20 +0700

    Update Solutions.js

--- src/pages/Solutions/Solutions.js
+++ src/pages/Solutions/Solutions.js
@@ -62,7 +62,7 @@ const solutions = () => {
     
 
        
-<div className='grid grid-cols-2 gap-1 gap-x- gap-y-5 md:grid-cols-2 lg:grid-cols-3 justify-center  py-5 mx-80 '>
+<div className='grid grid-cols-2 gap-1 gap-x- gap-y-5 md:grid-cols-2 lg:grid-cols-3 justify-center  py-5 mx-10 lg:flex lg:flex-row'>
 {cardData.map((item,) => (
     <div className='bg-white flex rounded-2xl mx-10 '>
         <div className='p-5 flex bgjustify-start w-full '>
@@ -96,7 +96,7 @@ const solutions = () => {
   
 
 
-<div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 ">
+<div class="gap-16 items-center py-8 px-20 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 ">
         <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-black">We didn't reinvent the wheel</h2>
             <p class="mb-4">We are strategists, designers and developers. Innovators and problem solvers. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need. Small enough to be simple and quick, but big enough to deliver the scope you want at the pace you need.</p>
